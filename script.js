0da904613563be606d4a04d4f7fc93fe357dba15
Ricardo Parson <54312175+bambooxv@users.noreply.github.com>
2020-12-15 09:12:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 let link = ["https://1.bp.blogspot.com/-0xgEwrIGW-4/XKIVTauckII/AAAAAAAABJw/-WpvmTst654EG08pT8o7xOe1CdCrODmrQCLcBGAs/s1600/emerald-ash-borer-jhahn.jpg", "https://www.renderhub.com/3dstudio/hercules-beetle/hercules-beetle-01.jpg", "https://eunis.eea.europa.eu/images/species//221/39f2ce9ea6b09b68b7b06e4f8dfdccba.jpg"];
 var item;
 
@@ -21,4 +23,5 @@ $(".add").click(function() {
 addItemToList() ;
     link.push(item);
     clearInput();
+});
 });
